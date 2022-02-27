3f5e2c8533f845cc8f16557d686518ee5503ec7b
Toni-Ty <Antonique2479@yahoo.com>
2022-02-26 23:20:17 -0600

    update app.components.ts with router and imports, constructor and method.

--- ramen-takeout-app/src/app/app.component.ts
+++ ramen-takeout-app/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'ramen-takeout-app';
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from './order.service';
+import { Order } from './Order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ramen-takeout-app';
+  orders: Order[];
+
+  constructor(private router: Router, private orderService: OrderService) {}
+  getOrders() {
+    this.orderService.getOrders().subscribe((data) => {
+      this.orders = data;
+    });
+  }
+
+  addOrder(): void {
+    this.router.navigate(['add-order']).then((e) => {
+      if (e) {
+        console.log('Navigation is successful!');
+      } else {
+        console.log('Navigation has failed!');
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((value) => {
+      this.getOrders();
+    });
+  }
 }

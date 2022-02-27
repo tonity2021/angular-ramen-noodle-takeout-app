9351fbdbc7664aae5960c61188b699b5738ebc56
Toni-Ty <Antonique2479@yahoo.com>
2022-02-27 03:51:33 -0600

    build reactive form and add to spec file

--- ramen-takeout-app/src/app/app.module.ts
+++ ramen-takeout-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { OrderService } from './order.service';
@@ -11,7 +12,9 @@ import { OrderService } from './order.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [OrderService],
   bootstrap: [AppComponent]
--- ramen-takeout-app/src/app/orders/orders.component.ts
+++ ramen-takeout-app/src/app/orders/orders.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { OrderService } from '../order.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
+
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private orderService: OrderService) {
+  }
 
-  ngOnInit(): void {
+  addForm: FormGroup;
+
+  ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [],
+      orderSize: ['', Validators.required],
+      specialRequest: ['', Validators.required]
+    });
+
+  }
+
+  onSubmit() {
+    this.orderService.addOrder(this.addForm.value)
+      .subscribe(data => {
+        this.router.navigate(['list-books']);
+      });
   }
 
 }
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
